37049c84d1befd37e4455a997bf04de7798456a5
HuyHung Dinh <hebitaxy@gmail.com>
2017-03-30 08:40:35 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# android-rewarded-video-ads
+The android app tests Admob rewarded video ads
+
+# About
+Created by HuyHung Dinh<br>
+GitHub: https://github.com/hu2di<br>
+Email: hebitaxy@gmail.com
---
+++ app/src/main/java/com/blogspot/hu2di/rewardedvideoads/MainActivity.java
@@ -0,0 +1,117 @@
+package com.blogspot.hu2di.rewardedvideoads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+
+public class MainActivity extends AppCompatActivity implements RewardedVideoAdListener {
+
+    private TextView tvStatus;
+    private Button btnWatch;
+
+    private RewardedVideoAd mAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+
+        initVideoAds();
+    }
+
+    private void initView() {
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+
+        btnWatch = (Button) findViewById(R.id.btnWatch);
+        btnWatch.setEnabled(false);
+        btnWatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAd.isLoaded()) {
+                    mAd.show();
+                }
+            }
+        });
+    }
+
+    private void initVideoAds() {
+        mAd = MobileAds.getRewardedVideoAdInstance(this);
+        mAd.setRewardedVideoAdListener(this);
+        loadAd();
+    }
+
+    private void loadAd() {
+        if (!mAd.isLoaded()) {
+            mAd.loadAd("ca-app-pub-3940256099942544/5224354917", new AdRequest.Builder().build());
+        }
+    }
+
+    @Override
+    public void onRewarded(RewardItem reward) {
+        Toast.makeText(this, "onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount(), Toast.LENGTH_SHORT).show();
+        tvStatus.setText("onRewarded! currency: " + reward.getType() + "  amount: " +
+                reward.getAmount());
+    }
+
+    @Override
+    public void onRewardedVideoAdLeftApplication() {
+        Toast.makeText(this, "onRewardedVideoAdLeftApplication", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdClosed() {
+        Toast.makeText(this, "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+        btnWatch.setEnabled(false);
+    }
+
+    @Override
+    public void onRewardedVideoAdFailedToLoad(int errorCode) {
+        Toast.makeText(this, "onRewardedVideoAdFailedToLoad", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoAdLoaded() {
+        Toast.makeText(this, "onRewardedVideoAdLoaded", Toast.LENGTH_SHORT).show();
+        btnWatch.setEnabled(true);
+    }
+
+    @Override
+    public void onRewardedVideoAdOpened() {
+        Toast.makeText(this, "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRewardedVideoStarted() {
+        Toast.makeText(this, "onRewardedVideoStarted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        mAd.resume(this);
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mAd.pause(this);
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mAd.destroy(this);
+        super.onDestroy();
+    }
+}
